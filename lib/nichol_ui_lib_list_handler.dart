dbe5ff1c2d72f7710a5362fd5a6cacfeeadf5b03
DavidNichol28 <mortymorticus@gmail.com>
2026-05-23 18:35:50 -0500

    removed riverpod from the app because theres no reason for it

--- lib/nichol_ui_lib_list_handler.dart
+++ lib/nichol_ui_lib_list_handler.dart
@@ -1,7 +1,6 @@
 
 library nichol_ui_lib_list_handler;
 
-export "package:flutter_riverpod/flutter_riverpod.dart";
 
 export "src/nichol_ui_lib_list_handler.dart";
 export "src/nichol_ui_lib_list_item.dart";
